d2a410b2c4d8007fa0f1d1bcb1d3bf70a8e1b1dd
edoardo1974 <edoardo.mozzato@gmail.com>
2025-07-05 13:43:58 +0100

    Last version

--- js/menudorestaurante.js
+++ js/menudorestaurante.js
@@ -1,15 +1,13 @@
 var listaComida = ["Fettuccine", "Ravioli", "Espaguete", "Lasagna", "Macarrão com queijo","Bacalhau salgado", "Sopa de feijão", "Robalo Assado", "Frango Assado", "Hamburger"];
 
-function pecaPrato()
+function pecaPrato()//Function to order a dish
 {
 let prato = prompt(`introduzir o primero prato`);
     return Number(prato);
 }
 
-function exibePrato(){
+function exibePrato(){//Function to exhibit the dish
 pratoum = pecaPrato();
 pratodois = pecaPrato();
-
-alert('Escolheu como primeiro prato ' + listaComida[pratoum] + ' e como segundo prato ' + listaComida[pratodois] + '.');
-
+alert('Escolheu como primeiro prato ' + listaComida[pratoum] + ' e como segundo prato ' + listaComida[pratodois] + '.');//
 }
